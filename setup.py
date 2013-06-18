a5abea0912e1d711044db0d4af3ae2b7566addf2
Dan Weaver <lastfinger@gmail.com>
2013-06-18 18:34:42 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (name='exoline',
+       version='0.1',
+       url = 'http://github.com/dweaver/exoline',
+       author = 'Dan Weaver',
+       author_email = 'danweaver@exosite.com',
+       description = 'Command line interface for Exosite platform.',
+       packages=['exoline'],
+       )
