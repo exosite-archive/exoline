642972b8f9b0551901df8f8e67d44b451249f6ff
Dan Weaver <danweaver@exosite.com>
2014-12-03 17:59:18 -0600

    copy build output to vm share if available (exosite-specific build machine thing)

--- windowsbuild.bat
+++ windowsbuild.bat
@@ -4,8 +4,14 @@ copy dist\exo\exo.exe dist\exo\exo
 python innosetup.py
 iscc innosetup.iss
 for /f "delims=" %%A in ('python -c "import exoline ; print(exoline.__version__)"') do @set exoline_version=%%A
-copy Output\setup.exe Output\exoline-%exoline_version%-setup.exe
-@echo Installer is here: 
-@echo Output\exoline-%exoline_version%-setup.exe
+if exist "E:\exoline_builds" (
+    copy Output\setup.exe E:\exoline_builds\exoline-%exoline_version%-setup.exe
+    @echo Installer is here: 
+    @echo E:\exoline_builds\exoline-%exoline_version%-setup.exe
+) else (
+    copy Output\setup.exe Output\exoline-%exoline_version%-setup.exe
+    @echo Installer is here: 
+    @echo Output\exoline-%exoline_version%-setup.exe
+)
 @echo Now would be a good time to push any changes to innosetup.iss:
 git diff
