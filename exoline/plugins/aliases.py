427232e3a82b178a85cc30bba7501004103d12df
Andrew Solz <andrewsolz@exosite.com>
2015-06-01 11:23:32 -0500

    Make autocomplete a plugin - add aliases, keys, and switches commands.

---
+++ exoline/plugins/aliases.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''Get dataport aliases from a CIK
+
+Usage:
+    exo aliases <cik>
+'''
+import re
+
+class Plugin():
+    def command(self):
+        return 'aliases'
+
+    def run(self, cmd, args, options):
+		rpc = options['rpc']
+		cik = options['cik']
+		aliases = rpc.info(cik, options={'aliases': True})['aliases']
+		print(" ".join(map(lambda x: str(x[0]), aliases.values())))
---
+++ exoline/plugins/keys.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''Get keys from ~/.exolinerc
+
+Usage:
+    exo keys
+'''
+import yaml
+import os
+
+class Plugin():
+    def command(self):
+        return 'keys'
+
+    def run(self, cmd, args, options):
+		conf = options['config'].config
+		print(" ".join(map(str, conf.get("keys", {}).keys())))
+		
+
